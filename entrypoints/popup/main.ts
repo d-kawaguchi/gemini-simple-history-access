2cee09800e77455b18e300e7c626d2e97fe684b6
d-kawaguchi <45481012+d-kawaguchi@users.noreply.github.com>
2024-05-28 15:45:52 +0900

    chore: migrate to wxt from vite-plugin-web-extension

--- entrypoints/popup/main.ts
+++ entrypoints/popup/main.ts
@@ -1,23 +1,23 @@
 import "./style.css";
-import viteLogo from "/vite.svg";
-import { setupCounter } from "./counter.ts";
-import typescriptLogo from "./typescript.svg";
+import typescriptLogo from "@/assets/typescript.svg";
+import { setupCounter } from "@/components/counter";
+import viteLogo from "/wxt.svg";
 
 // biome-ignore lint/style/noNonNullAssertion: サンプルコードのため
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+    <a href="https://wxt.dev" target="_blank">
+      <img src="${viteLogo}" class="logo" alt="WXT logo" />
     </a>
     <a href="https://www.typescriptlang.org/" target="_blank">
       <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
     </a>
-    <h1>Vite + TypeScript</h1>
+    <h1>WXT + TypeScript</h1>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
     <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
+      Click on the WXT and TypeScript logos to learn more
     </p>
   </div>
 `;
--- vite.config.ts
+++
@@ -1,7 +0,0 @@
-// vite.config.ts
-import { defineConfig } from "vite";
-import webExtension from "vite-plugin-web-extension";
-
-export default defineConfig({
-    plugins: [webExtension()],
-});
